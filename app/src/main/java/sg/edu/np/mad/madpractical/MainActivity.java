b66c76a0522c16cdb33cc7796b764423178f6e74
Yong Zi Ren <s10219524@connect.np.edu.sg>
2022-04-28 22:11:33 +0800

    edit

--- app/src/main/java/sg/edu/np/mad/madpractical/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/madpractical/MainActivity.java
@@ -7,22 +7,24 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
-    User user = new User("Tom", "description", 1, false);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    public void onClick(View v) {
+        User user = new User("Tom", "description", 1, false);
         Button Btn = findViewById(R.id.FOLLOW);
-        if (!user.Followed) {
-            Btn.setText("UNFOLLOW");
-            user.Followed = true;
-        }
-        else{
-            Btn.setText("FOLLOW");
-            user.Followed = false;
-        }
+        Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!user.Followed) {
+                    Btn.setText("UNFOLLOW");
+                    user.Followed = true;
+                }
+                else{
+                    Btn.setText("FOLLOW");
+                    user.Followed = false;
+                }
+            }
+        });
     }
 }
